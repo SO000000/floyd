1a3c3d38d109249c0310e9ccdc9379398a77a4b6
sou <hirawatasou@gmail.com>
2022-06-26 15:21:36 +0900

    Populationクラスのデストラクタの修正

--- Population.py
+++ Population.py
@@ -11,7 +11,12 @@ class Population:
             self.ind[i] = Individual()
             self.nextind[i] = Individual()
 
+    #デストラクタ
     def __del__(self):
+
+        self.ind = ind
+        self.nextind = nextind
+
         for i in range(0,Individual.POP_SIZE):
             del self.ind[i]
             del self.nextind[i]
