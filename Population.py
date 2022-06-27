bf0f353416a3dfad8589a8740c996593c5b8f042
sou <hirawatasou@gmail.com>
2022-06-27 14:31:17 +0900

    Populationクラスのevaluate関数の定義

--- Population.py
+++ Population.py
@@ -7,24 +7,29 @@ class Population:
     # コンストラクタ
     def __init__(self):
 
-        self.ind = []       #現世代個体群
-        self.nextind = []   #次世代個体群
+        self.ind = []  # 現世代個体群
+        self.nextind = []  # 次世代個体群
 
         for i in range(0, Individual.POP_SIZE):
 
-            #現世代個体群を生成
+            # 現世代個体群を生成
             self.x = Individual.Individual()
             self.ind.append(self.x.chrom)
 
-            #次世代個体群を生成
+            # 次世代個体群を生成
             self.y = Individual.Individual()
             self.nextind.append(self.y.chrom)
 
     # デストラクタ
-    def __del__(ind,nextind):
+    def __del__(ind, nextind):
 
         for i in range(0, Individual.POP_SIZE):
             del ind[i]
             del nextind[i]
         # メモリを解放
         gc.collect()
+
+    def evaluete(self):
+        for i in range(0, Individual.POP_SIZE):
+            self.ind[i].fitness = Individual.evaluate(self.ind[i])
+        sort(0, Individual.POP_SIZE - 1)
